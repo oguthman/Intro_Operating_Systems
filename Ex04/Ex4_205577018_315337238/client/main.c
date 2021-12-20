16486f17442453f41e4231b66c885837f53b9c6c
shaharmorag <shahar.sdm@gmail.com>
2021-12-20 19:27:43 +0200

    creating new project structure

---
+++ Ex04/Ex4_205577018_315337238/client/main.c
@@ -0,0 +1,115 @@
+#define _CRT_SECURE_NO_WARNINGS
+/*!
+******************************************************************************
+\file main.c
+\date 20 December 2021
+\authors Shahar Dorit Morag 315337238 & Ofir Guthman 205577018
+\project #4
+\brief 
+
+\details
+
+\par Copyright
+(c) Copyright 2021 Ofir & Shahar
+\par
+ALL RIGHTS RESERVED
+*****************************************************************************/
+
+/************************************
+*      include                      *
+************************************/
+#include "..\message_defs.h"
+//
+#include <stdio.h>
+#include <string.h>
+
+/************************************
+*      definitions                 *
+************************************/
+
+/************************************
+*       types                       *
+************************************/
+
+/************************************
+*      variables                    *
+************************************/
+static struct {
+	char* server_ip;
+	char* server_port;
+	char* username;
+} gs_inputs;
+
+/************************************
+*      static functions             *
+************************************/
+static void parse_arguments(int argc, char* argv[]);
+static void validate_menu_input(int* value, int min_arg, int max_arg, char* message);
+static void clear_buffer();
+
+/************************************
+*       API implementation          *
+************************************/
+int main(int argc, char* argv[])
+{
+	// parse arguments
+	parse_arguments(argc, argv);
+	
+	int answer_to_reconnect = 0;
+	
+	do
+	{
+		// connect to server
+		// if succeeded
+		{
+			printf("Connected to server on <ip>:<port>\n");
+			//break;
+		}
+		// if not succeeded
+		printf("Failed connecting to server on <ip>:<port>\nChoose what to do next:\n1. Try to reconnect\n2. Exit\n");
+		scanf_s("%d", &answer_to_reconnect);
+		validate_menu_input(&answer_to_reconnect, 1, 2, "Choose what to do next:\n1. Try to reconnect\n2. Exit\n");
+
+	} while (answer_to_reconnect == 1);
+	
+
+}
+
+/************************************
+* static implementation             *
+************************************/
+/// Description: parse arguments and open files.  
+/// Parameters: 
+///		[in] argc - number of arguments. 
+///		[in] argv - arguments list. 
+/// Return: none.
+static void parse_arguments(int argc, char* argv[])
+{
+	//// check if there are enough arguments
+	//ASSERT(argc == 4, "Error: not enough arguments.\n");
+
+	//// parse arguments
+	//gs_argument_inputs.virtual_memory_size = 1 << (strtol(argv[1], NULL, 10) - PAGE_SIZE);
+	//gs_argument_inputs.physical_memory_size = 1 << (strtol(argv[2], NULL, 10) - PAGE_SIZE);
+	//gs_argument_inputs.input_file = File_Open(argv[3], "r");
+	//ASSERT(gs_argument_inputs.input_file != NULL, "Error: Can't open input file\n");
+}
+
+// TODO: fix the string as an input
+static void validate_menu_input(int* value, int min_arg, int max_arg, char* message)
+{
+	while (*value > max_arg || *value < min_arg)
+	{
+		char input[20];
+		printf("Error: Illegal command\n");
+		//clear_buffer();
+		printf("%s", message);
+		gets(input);
+		*value = strtol(input, NULL, 10);
+	}
+}
+
+static void clear_buffer()
+{
+	fseek(stdin, 0, SEEK_END);
+}
---
+++ Ex04/Ex4_205577018_315337238/message_defs.h
@@ -0,0 +1,69 @@
+/*!
+******************************************************************************
+\file message_defs.h
+\date 20 December 2021
+\author Shahar Dorit Morag & Ofir Guthman
+\project #4
+\brief 
+
+\details
+
+\par Copyright
+(c) Copyright 2021 Ofir & Shahar
+\par
+ALL RIGHTS RESERVED
+*****************************************************************************/
+
+#ifndef __MESSAGE_DEFS_H__
+#define __MESSAGE_DEFS_H__
+
+/************************************
+*      include                      *
+************************************/
+
+/************************************
+*      definitions                 *
+************************************/
+
+/************************************
+*       types                       *
+************************************/
+typedef enum {
+	MESSAGE_TYPE_CLIENT_REQUEST,
+	MESSAGE_TYPE_CLIENT_VERSUS,
+	MESSAGE_TYPE_CLIENT_PLAYER_MOVE,
+	MESSAGE_TYPE_CLIENT_DISCONNECT,
+	//
+	MESSAGE_TYPE_SERVER_APPROVED,
+	MESSAGE_TYPE_SERVER_DENIED,
+	MESSAGE_TYPE_SERVER_MAIN_MENU,
+	MESSAGE_TYPE_GAME_STARTED,
+	MESSAGE_TYPE_TURN_SWITCH,
+	MESSAGE_TYPE_SERVER_MOVE_REQUEST,
+	MESSAGE_TYPE_GAME_ENDED,
+	MESSAGE_TYPE_SERVER_NO_OPPONENTS,
+	MESSAGE_TYPE_GAME_VIEW,
+	MESSAGE_TYPE_SERVER_OPPONENT_QUIT
+} e_message_type;
+
+/************************************
+*       API                         *
+************************************/
+
+/*!
+******************************************************************************
+\brief
+Initialize func 
+
+\details
+Must be called only once
+
+\param
+ [in] counter_val - reset counter value
+ [out] out_val    - 
+
+\return none
+*****************************************************************************/
+
+
+#endif //__MESSAGE_DEFS_H__
