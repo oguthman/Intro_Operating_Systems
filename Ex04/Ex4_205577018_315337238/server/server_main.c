4f9009bc995080d7a042fda335a2c117509556ee
shaharmorag <shahar.sdm@gmail.com>
2021-12-27 20:39:48 +0200

    game logic

--- Ex04/Ex4_205577018_315337238/server/server_main.c
+++ Ex04/Ex4_205577018_315337238/server/server_main.c
@@ -21,6 +21,7 @@ ALL RIGHTS RESERVED
 #include <string.h>
 #include <stdio.h>
 #include <stdint.h>
+#include <stdbool.h>
 //
 #include "../shared/socket_handle.h"
 
@@ -62,10 +63,17 @@ int main(int argc, char* argv[])
 	SOCKET server_socket = Socket_Init(socket_server, SERVER_IP, g_port);
 	ASSERT(server_socket != INVALID_SOCKET, "Error: server can't open socket\n");		//TODO: exit?
 
-	// lesten on socket
+	// listen on socket
+	uint8_t number_of_clients_connected = 0;
 	ASSERT(listen(server_socket, SOMAXCONN) != SOCKET_ERROR, "Error: failed listening on socket %ld\n", WSAGetLastError());		//TODO: action - socket cleanup Socket_TearDown()
 
-	
+	// accept or decline. create new thread for each client. TODO: WHILE LOOP on number_of_clients_connected
+	SOCKET accept_socket = accept(server_socket, NULL, NULL);
+	ASSERT(accept_socket != INVALID_SOCKET, "Error: server can't open socket for client\n");		//TODO: exit?
+	number_of_clients_connected++;
+
+	//if number_of_clients_connected ==2, decline new client by connecting and sending decline message
+
 }
 
 /************************************
@@ -86,21 +94,35 @@ static void parse_arguments(int argc, char* argv[])
 
 static uint32_t g_game_counter = 0;
 
-//static void temp_game(int move_opponent, char* username, int last_number_before_boom)
-//{
-//	int move_self, current_move;
-//	if (move_opponent != "boom")
-//	{
-//		current_move = 1 + move_opponent;
-//		if ((current_move % 7) == 0 || ((current_move / 10) % 7) == 0)
-//		{
-//			//current_move = "boom";
-//		}
-//	}
-//
-//	else
-//	{
-//		current_move = last_number_before_boom + 2;
-//	}
-//
-//}
+// return true if the game is still on, false if player lost in this turn.
+static bool temp_game_logic(char** move_self)
+{
+	uint32_t correct_move, units;
+	bool boom = false;
+	
+	correct_move = 1 + g_game_counter;//TODO: decide when to tik
+	units = correct_move % 10;
+
+	if ((correct_move % 7) == 0 || (units % 7) == 0)
+	{
+		boom = true;
+	}
+	
+	printf("your move\n");
+	scanf("%s\n", *move_self);	//with or without \n
+
+	if (boom)
+	{
+		if (strcmp("boom", *move_self)) return true;
+		return false;
+	}
+	else
+	{
+		uint32_t number = strtol(*move_self, NULL, 10);
+		if (number == correct_move) return true;
+	}
+	return false;
+
+
+
+}
